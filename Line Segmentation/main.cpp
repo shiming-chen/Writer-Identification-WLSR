8438cd3ae9073328683e5bb5ed3fbef09b9fe764
Shiming Chen <m13648521576@163.com>
2018-07-27 22:23:15 +0800

    Line segmentation

---
+++ Line Segmentation/main.cpp
@@ -0,0 +1,29 @@
+#include "LineSegmentation.hpp"
+#include <string>
+
+int main(int argc, char *argv[]) {
+	
+    string img_path = argv[1];
+    string imgname=img_path;
+    int pos=imgname.find_last_of("/");
+    imgname=imgname.substr(pos+1);
+    imgname=imgname.substr(0,imgname.find_last_of("."));
+    LineSegmentation line_segmentation(img_path);
+    vector<cv::Mat> lines = line_segmentation.segment();
+    int idx = 0;
+    for(auto m : lines) {
+        imwrite(imgname+"_Line " + to_string(idx++) + ".jpg", m);
+	
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
